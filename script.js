4fb0576bedc17e598db90763d487383e6a6429e1
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-13 12:13:39 -0600

    changed HTML/format to match

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 TODO: date at the top of the calendar
 TODO: timeblocks for business hours
 TODO: color-coded for past, present, future (in CSS)
-TODO: 
-TODO: 
-TODO: 
-TODO: 
+TODO: timeblock can add events
+TODO: events can be saved 
+TODO: once saved, recalled upon refresh (local storage)
+*/
