a8e2dec93a901ecbe581d4e057bd8cd585687b5f
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-14 17:49:04 -0600

    added js info and assigned hours

--- script.js
+++ script.js
@@ -14,3 +14,57 @@ var setTime = dayjs().set("hour", 9);
 var businessHours = setTime++;
 console.log(setTime);
 console.log(businessHours);
+
+var hours = [
+  document.getElementById("9"),
+  document.getElementById("10"),
+  document.getElementById("11"),
+  document.getElementById("12"),
+  document.getElementById("1"),
+  document.getElementById("2"),
+  document.getElementById("3"),
+  document.getElementById("4"),
+  document.getElementById("5"),
+];
+
+var test = document.querySelector(".description");
+var retrieve = localStorage.getItem("test");
+var save = document.querySelector("saveBtn");
+
+if (save) {
+  save.addEventListener("click", function () {
+    localStorage.setItem("input", test.textContent);
+    console.log(test.textContent);
+  });
+}
+
+//set the time for 9-5 business hours
+// var nine = {
+//   hours[0]:
+//     dayjs().set("hour", 9).set("minute", 00).set("second", 00),
+// };
+// console.log(nine);
+// var ten = dayjs().set("hour", 10).set("minute", 00).set("second", 00);
+// console.log(ten);
+// var ten = dayjs().set("hour", 11).set("minute", 00).set("second", 00);
+// console.log(ten);
+// var eleven = dayjs().set("hour", 12).set("minute", 00).set("second", 00);
+// console.log(eleven);
+// var twelve = dayjs().set("hour", 13).set("minute", 00).set("second", 00);
+// console.log(twelve);
+// var one = dayjs().set("hour", 14).set("minute", 00).set("second", 00);
+// console.log(one);
+// var two = dayjs().set("hour", 15).set("minute", 00).set("second", 00);
+// console.log(two);
+// var three = dayjs().set("hour", 16).set("minute", 00).set("second", 00);
+// console.log(three);
+// var four = dayjs().set("hour", 17).set("minute", 00).set("second", 00);
+// console.log(four);
+// var five = dayjs().set("hour", 18).set("minute", 00).set("second", 00);
+// console.log(five);
+
+// var entry = document.querySelectorAll(".description").textContent;
+// var saveBtn = document.querySelectorAll(".saveBtn");
+// saveBtn.on("click", function () {
+//   localStorage.setItem("input", entry);
+// });
