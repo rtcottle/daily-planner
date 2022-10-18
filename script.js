90c9ad99390b54da16b624328a721b1ca0a65cdc
Ryan C <111948791+GreyJedi17@users.noreply.github.com>
2022-10-17 23:00:06 -0600

    removed some comments, added more helpful ones

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ var businessHours = setTime++;
 console.log(setTime);
 console.log(businessHours);
 
+//targets the entire div with hour and description
 var hours = [
   document.getElementById("hour-9"),
   document.getElementById("hour-10"),
@@ -27,16 +28,29 @@ var hours = [
   document.getElementById("hour-5"),
 ];
 
+//targets the descriptions objects for each hour.
+var entries = [
+  $("#9"),
+  $("#10"),
+  $("#11"),
+  $("#12"),
+  $("#1"),
+  $("#2"),
+  $("#3"),
+  $("#4"),
+  $("#5"),
+];
+
 var events = {
   hour9: hours[0].value,
-  // hour10: hours[1].value,
-  // hour11: hours[2].value,
-  // hour12: hours[3].value,
-  // hour1: hours[4].value,
-  // hour2: hours[5].value,
-  // hour3: hours[6].value,
-  // hour4: hours[7].value,
-  // hour5: hours[8].value,
+  hour10: hours[1].value,
+  hour11: hours[2].value,
+  hour12: hours[3].value,
+  hour1: hours[4].value,
+  hour2: hours[5].value,
+  hour3: hours[6].value,
+  hour4: hours[7].value,
+  hour5: hours[8].value,
 };
 
 console.log(events);
@@ -46,17 +60,34 @@ var $saveEl = $(".saveBtn");
 
 //adds function to each save button, sets local storage
 $(".saveBtn").click(function () {
-  localStorage.setItem("Event", JSON.stringify(events));
+  console.log($(this).siblings(".description").val());
+  localStorage.setItem(
+    //TODO get the selector here!!,
+    JSON.stringify($(this).siblings(".description").val())
+  );
   // userItems();
-  renderInputs();
-  console.log(events);
+
+  //TODO: google js/Jquery
+  //upon clicking save, grab value of ID associated with time block, that will be name/key of 1st argument of .setItem
+  //grab value of text area, assign that as second argument of .setItem (don't forget JSON.stringify).
+  //(line 63) save to storage.
+
+  // renderInputs();
 });
 
-//retrieves user inputs
+//retrieves user inputs - - s
 function renderInputs() {
   var lastEvent = JSON.parse(localStorage.getItem("Event"));
   if (lastEvent !== null) {
-    events = $(".description").val();
+    events.hour9 = $("#9").val(events.hour9);
+    events.hour10 = $(".description").val();
+    events.hour11 = $(".description").val();
+    events.hour12 = $(".description").val();
+    events.hour1 = $(".description").val();
+    events.hour2 = $(".description").val();
+    events.hour3 = $(".description").val();
+    events.hour4 = $(".description").val();
+    events.hour5 = $(".description").val();
     // $(".description").value = events;
     console.log(events);
   }
